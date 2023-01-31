e1873161617f11f9340b19f7f5eda6b1560806af
gregorni <119804311+gregorni@users.noreply.github.com>
2023-01-31 18:14:03 +0100

    Add explaining text when creating new profile

--- src/preferences_window.py
+++ src/preferences_window.py
@@ -342,7 +342,7 @@ class CavalierPreferencesWindow(Adw.PreferencesWindow):
         self.profile_new_button_add.connect('clicked', \
             self.create_color_profile)
         self.profile_new_box.append(self.profile_new_button_add)
-        self.profile_new_label = Gtk.Label.new('')
+        self.profile_new_label = Gtk.Label.new(_('The new profile will be a copy of the active profile.'))
         self.profile_add_box.append(self.profile_new_label)
         self.profile_remove_button = Gtk.MenuButton.new()
         self.profile_remove_button.set_icon_name('list-remove-symbolic')
